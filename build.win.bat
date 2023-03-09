916e80bb8315848f8ee8ef445e465c6789305cc4
Gustav Gonzalez <xtingray@gmail.com>
2023-03-09 07:51:09 -0500

    Updating Windows script

--- build.win.bat
+++ build.win.bat
@@ -1,7 +1,7 @@
 rmdir /Q /s build
 mkdir build
 cd build
-cmake -G "MinGW Makefiles" ..
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="C:/Quazip" -DQUAZIP_USE_QT_ZLIB=ON  ..
 make
 rmdir /Q /s C:\Quazip 
 mkdir C:\Quazip
